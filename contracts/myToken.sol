b2ae1d3aaeefc842697d9e91fd8cb9d1f271447d
pelzfx <111582303+pelzfx@users.noreply.github.com>
2023-03-18 12:39:06 +0100

    creaed token mapping

--- contracts/myToken.sol
+++ contracts/myToken.sol
@@ -6,4 +6,7 @@ contract myToken{
     string public name ="Pelumi's Token";
     string public symbol = "PT";
     uint256 public totalSupply = 4000000;
+
+    address public owner;
+    mapping(address => uint256) balances;
 }
